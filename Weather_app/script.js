d8e0154dd79133bd4a58c7dda372c3ef5e223c0f
corronwarren2 <86121210+corronwarren2@users.noreply.github.com>
2021-12-02 22:21:50 -0500

    first commit

---
+++ Weather_app/script.js
@@ -0,0 +1,42 @@
+let weather = {
+    "apiKey": "70a2e314addb568e7ba516b81c9c610b",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=imperial&appid=" 
+        + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon , description} = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = " humidity " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed:" + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document
+.querySelector(".search button")
+.addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+ if (event.key == "Enter") {
+     weather.search();
+ }
+});
+
+weather.fetchWeather("Tampa");
